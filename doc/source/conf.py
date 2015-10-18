7d05907d11adb6b0d242e498a4b44355870e4034
vsoch <vsochat@stanford.edu>
2015-10-18 14:57:59 -0700

    modified:   doc/source/_static/custom.css 	modified:   doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -123,6 +123,7 @@ if os.environ.get('READTHEDOCS', None) is None:
   html_theme = "sphinx_rtd_theme"
   html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_style = 'css/custom.css'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
