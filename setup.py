96713807e15cbe0a57c6393af8a8a2b924a54ba1
vsoch <vsochat@stanford.edu>
2016-02-04 17:08:21 -0800

    adding seaborn

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     description="Python module for managing experiment factory experiments, for deployment to a psiturk battery or docker container.",
     keywords='psiturk behavior neuroscience experiment factory docker',
 
-    install_requires = ['numpy','Flask','gitpython','flask-restful','selenium','cognitiveatlas','scipy','numexpr'],
+    install_requires = ['numpy','Flask','gitpython','flask-restful','selenium','cognitiveatlas','scipy','numexpr','seaborn'],
 
     entry_points = {
         'console_scripts': [
